06345e8896a8f207970eeb25483b3cc826945c2b
Cory <Coryf65@gmail.com>
2020-02-26 20:53:31 -0600

    calling the api, and getting data

---
+++ api.py
@@ -0,0 +1,35 @@
+#using python to call api's using a free api ()
+
+import requests
+import json
+
+base_url = 'https://api.upcitemdb.com/prod/trial/lookup'
+
+parameters = {'upc' : '012993441012'}
+
+response = requests.get(base_url, params=parameters)
+
+# Testing thye url building
+print(response.url)
+
+# data comes as JSON, which is not built into python
+# need to import a package, using json module
+content = response.content
+
+info = json.loads(content)
+
+# see what type of reponse
+print(type(info))
+print(info)
+
+# getting the data we want
+item = info['items']
+
+item_info = item[0]
+
+title = item_info['title']
+
+brand = item_info['brand']
+
+print(title)
+print(brand)
